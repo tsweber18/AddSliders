6829b0c999d8bbef04fba1fbde5341e20add54b5
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-03-19 18:47:37 +0000

    Troubleshooting errors in YOUR CODE ERIC :P

--- scripts.js
+++ scripts.js
@@ -41,69 +41,70 @@ var fib = function(n, node){
 };
 
 var style = document.createElement('style');
-style.textContent=
-    "#fib {" +
-    "   display: inline-block;" +
-    "   width: 20000px;" +
-    "}"+
-    "" +
-    ".fib{"+
-    "   background-color:rgba{0,0,255,0.1);"+
-    "}" +
-    ""+
-    ".fib-left {" +
-    "   float:left;"+
-    "   display: inline-block;" +
-    "   margin-right: 4px;"+
-    "}"+
-    ""+
-    ".fib-right {" +
-	"	float: right;" +
-	"	display: inline-block;" +
-	"	margin-left: 4px;" +
-	"}" +
-	"" +
-	".shadowed {" +
-	"	text-shadow: 1px 1px 2px black;" +
-	"	color:       white;" +
-	"}" +
-	".stuff-box {" +
-	"	font-family: 'helvetica neue', helvetica, sans-serif;" +
-	"	letter-spacing: 1px;" +
-	"	text-transform: capitalize;" +
-	"	text-align: center;" +
-	"	padding: 3px 10px;" +
-	"	margin: 10px;" +
-	"	cursor: pointer;" +
-	"	border-radius: 10px;" +
-	"	border-width: 2px;" +
-	"	border-style: solid;" +
-	"}" +
-	"" +
-	".red {" +
-	"	border-color: rgb(255,0,0);" +
-	"	background:   rgb(180,60,60);" +
-	"	box-shadow: 1px 1px 2px rgba(200,0,0,0.4);" +
-	"}" +
-	"" +
-	".yellow {" +
-	"	border-color: rgb(255,255,0);" +
-	"	background:   rgb(180,180,60);" +
-	"	box-shadow: 1px 1px 2px rgba(200,200,0,0.4);" +
-	"}" +
-	"" +
-	".blue {" +
-	"	border-color: rgb(0,0,255);" +
-	"	background:   rgb(60,60,180);" +
-	"	box-shadow: 1px 1px 2px rgba(0,0,200,0.4);" +
-	"}" +
-	"" +
-	".green {" +
-	"	border-color: rgb(0,255,0);" +
-	"	background:   rgb(60,180,60);" +
-	"	box-shadow: 1px 1px 2px rgba(0,200,0,0.4);" +
-	"}";
 
+var styleText =  "#fib {" ;
+styleText+=    "   display: inline-block;" ;
+styleText+=    "   width: 20000px;" ;
+    styleText+="}";
+    styleText+="" ;
+    styleText+=".fib{";
+    styleText+="   background-color:rgba(0,200,200,0.1);";
+    styleText+="}" ;
+    styleText+="";
+    styleText+=".fib-left {" ;
+    styleText+="   float:left;";
+styleText+=  "   display: inline-block;" ;
+styleText+=  "   margin-right: 4px;";
+styleText+=  "}";
+styleText+=  "";
+styleText+=  ".fib-right {" ;
+styleText+=	"	float: right;" ;
+styleText+=	"	display: inline-block;" ;
+styleText+=	"	margin-left: 4px;" ;
+styleText+=	"}" ;
+styleText+=	"" ;
+styleText+=	".shadowed {" ;
+styleText+=	"	text-shadow: 1px 1px 2px black;" ;
+styleText+=	"	color:       white;" ;
+styleText+=	"}" ;
+styleText+=	".stuff-box {" ;
+styleText+=	"	font-family: 'helvetica neue', helvetica, sans-sif;"; 
+styleText+=	"	letter-spacing: 1px;" ;
+styleText+=	"	text-transform: capitalize;" ;
+styleText+=	"	text-align: center;" ;
+styleText+=	"	padding: 3px 10px;" ;
+styleText+=	"	margin: 10px;" ;
+styleText+=	"	cursor: pointer;" ;
+styleText+=	"	border-radius: 10px;" ;
+styleText+=	"	border-width: 2px;" ;
+styleText+=	"	border-style: solid;" ;
+styleText+=	"}" ;
+styleText+=	"" ;
+styleText+=	".red {" ;
+styleText+=	"	border-color: rgb(255,0,0);" ;
+styleText+=	"	background:   rgb(180,60,60);" ;
+//styleText+=	"	box-shadow: 1px 1px 2px rgba(200,0,0,0.4);" ;
+styleText+=	"}" ;
+styleText+=	"" ;
+styleText+=	".yellow {" ;
+styleText+=	"	border-color: rgb(255,255,0);" ;
+styleText+=	"	background:   rgb(180,180,60);" ;
+//styleText+=	"	box-shadow: 1px 1px 2px rgba(200,200,0,0.4);" ;
+styleText+=	"}" ;
+styleText+=	"" ;
+styleText+=	".blue {" ;
+styleText+=	"	border-color: rgb(0,0,255);" ;
+styleText+=	"	background:   rgb(60,60,180);" ;
+//styleText+=	"	box-shadow: 1px 1px 2px rgba(0,0,200,0.4);" ;
+styleText+=	"}" ;
+styleText+=	"" ;
+styleText+=	".green {" ;
+styleText+=	"	border-color: rgb(0,255,0);" ;
+styleText+=	"	background:   rgb(60,180,60);" ;
+//styleText+=	"	box-shadow: 1px 1px 2px rgba(0,200,0,0.4);";
+styleText+=	"}";
+
+style.textContent= styleText;
 document.querySelector('body').appendChild(style);
 
 (function(color, id){
